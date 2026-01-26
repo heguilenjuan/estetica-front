a841efeac9c44b775be5f3bc25836e73f098160f
heguilenjuan <heguilenjuan@gmail.com>
2026-01-26 16:22:11 -0300

    init --project and create some pages

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Estetica app</h1>
+      </header>
+      <main>
+        <form>
+          <div>
+            <label>*Usuario: </label>
+            <input />
+          </div>
+          <div>
+            <label>*Contraseña: </label>
+            <input />
+          </div>
+          <div>
+              <button>Ingresar</button>
+          </div>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default App
